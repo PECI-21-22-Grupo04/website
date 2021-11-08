3edc7556c093385ab908b97cf8cc47a1a681ffbd
FilipeAdao96 <filipeadao@ua.pt>
2021-11-08 20:55:50 +0000

    added about us page, minor changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.render('home')
 })
-app.get('/contact', (req, res) => {
+app.get('/about', (req, res) => {
   res.render('contact')
 })
 
